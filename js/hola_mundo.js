1de0868fd5f03ac2e3a91f7ae53f109152b5d707
daniel <danielrinconbravo40@gmail.com>
2022-02-07 17:25:00 -0500

    descuentos

--- js/hola_mundo.js
+++ js/hola_mundo.js
@@ -7,7 +7,7 @@ if(nombre){
 console.log ("bienvenido"+" "+nombre)
 };
 // tercer ejercicio
-let costoProducto = 22000;
+let costoProducto = 50000;
 let domicilio = 5000;
 let impuesto =0.10;
 let subTotal = costoProducto + (costoProducto*impuesto);
@@ -15,3 +15,22 @@ let costoTotal = subTotal + domicilio;
 console.log("el costo del producto es de"+" "+costoProducto)
 console.log("subtotal del producto"+" "+subTotal);
 console.log("el costo total seria"+" "+costoTotal);
+// cuarto ejercicio
+var costoInicial = 60000
+var primerDesc = 0
+var segdesc = 0.05
+var tercdesc = 0.1
+var producto1 = costoInicial-(costoInicial*segdesc)
+var producto2 = costoInicial-(costoInicial*tercdesc)
+console.log("el valor inicial es de"+" "+costoInicial)
+if (costoInicial<30000){
+    console.log("sin descuento" )
+}
+else if(costoInicial>30000 && costoInicial<100000){
+    console.log("valor final"+" "+producto1)
+}
+else if (costoInicial>=100000){
+    console.log("valor final"+" "+producto2)
+}
+
+
