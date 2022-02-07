b61f5c5ed4a2c63805a631a85b56b9131938b732
daniel <danielrinconbravo40@gmail.com>
2022-02-07 16:16:12 -0500

    total del producto

--- js/hola_mundo.js
+++ js/hola_mundo.js
@@ -6,3 +6,12 @@ let nombre ="(sr)Daniel Bravo";
 if(nombre){
 console.log ("bienvenido"+" "+nombre)
 };
+// tercer ejercicio
+let costoProducto = 22000;
+let domicilio = 5000;
+let impuesto =0.10;
+let subTotal = costoProducto + (costoProducto*impuesto);
+let costoTotal = subTotal + domicilio;
+console.log("el costo del producto es de"+" "+costoProducto)
+console.log("subtotal del producto"+" "+subTotal);
+console.log("el costo total seria"+" "+costoTotal);
